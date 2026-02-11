8a80b59934f3bb6d1aab3929b4cb0ba2a985ff31
김유진 <salt101050@gmail.com>
2026-02-11 22:41:34 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.06 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+func solution(_ my_string:String, _ k:Int) -> String {
+     
+    var ztring = ""
+    for g1 in 1...k { 
+        
+        ztring = ztring + my_string 
+    }
+return ztring    
+    
+}
