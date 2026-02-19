9248ee5f23a2aa917691c253cdeffe83a79bd18b
김유진 <salt101050@gmail.com>
2026-02-19 10:02:55 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.03 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.swift
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.swift
@@ -1,5 +1,16 @@
 import Foundation
 
 func solution(_ num_list:[Int]) -> [Int] {
-    return []
+    var list = num_list
+    
+    let last = list[list.count - 1]
+    let prev = list[list.count - 2]
+    
+    if last > prev {
+        list.append(last - prev)
+    } else {
+        list.append(last * 2)
+    }
+    
+    return list
 }
